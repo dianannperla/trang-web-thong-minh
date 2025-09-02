cf86f06f95409d0ef6050c5e2fa8e414b8d7a5bd
dianannperla <waafirmelyza@hotmail.com>
2025-09-02 17:43:30 +0100

    Improve welcome message to enhance user experience

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('Welcome to the Smart Web application!');
+    console.log('Welcome to the Smart Web application! We are glad to have you here.');
 });
